3884d922d1102ba03e7ca60ce6f9f01d31505ff4
Chase Clettenberg <clettenberg@hey.com>
2024-03-09 13:57:01 -0600

    [feat] add topics endpoint

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/topics", (req, res) => {
+  const cursor = req.query.cursor || "start";
+  const limit = req.query.limit || 10;
+
+  res.status(200).json({
+    list: [
+      {
+        name: "Fred Flintstone",
+        email: "fred.flinstone@quarry.com",
+        topic: "The future of stone age automobiles",
+        summary: "It's time to put our feet down and move forward",
+        category: "talk",
+      },
+    ],
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`services-proposals now listening on port ${port}`);
 });
