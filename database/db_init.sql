ab9bf264c3a7d474ae48ba9f90d175e2d77ac6e5
zenlex <erichrkeil@gmail.com>
2024-04-20 03:20:42 -0500

    feat(db): add trigger to update modified timestamps

--- database/db_init.sql
+++ database/db_init.sql
@@ -1,3 +1,10 @@
+CREATE OR REPLACE FUNCTION trigger_set_updated() RETURNS TRIGGER AS $$
+BEGIN
+	NEW.updated = NOW();
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE if NOT EXISTS proposals (
 	title varchar(100) NOT NULL,
 	summary TEXT NOT NULL,
@@ -7,4 +14,9 @@ CREATE TABLE if NOT EXISTS proposals (
 	id SERIAL PRIMARY KEY,
 	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	updated TIMESTAMP
-)
+);
+
+CREATE OR REPLACE TRIGGER set_updated
+BEFORE UPDATE ON proposals
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_updated();
