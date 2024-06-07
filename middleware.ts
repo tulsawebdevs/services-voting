343c0bde496aad66ec5e042d4ed9a561d46489b8
rbueno <133924558+randomontherun@users.noreply.github.com>
2024-06-06 19:22:07 -0500

    feat: add authentication middleware (#48)
    * feat: add authentication middleware
    
    * fix(auth): use base64 encoding/decoding for jwt key

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import {Request, Response, NextFunction} from "express";
+import jwt from "jsonwebtoken";
+import {TokenPayload} from "./index";
+
+async function clerkAuth(req : Request, res : Response, next : NextFunction) {
+    const token = req.headers.authorization?.replace("Bearer: ", "");
+    const base64Key = process.env.CLERK_JWT_KEY as string;
+    const publicKey = Buffer.from(base64Key, 'base64').toString('ascii');
+    const whitelist = [
+        { method: 'GET', route: '/proposals' }
+    ];
+    const isWhitelisted = whitelist.some(
+        (item) => item.method === req.method && item.route === req.path
+    );
+    if (isWhitelisted) {
+        req.user = {} as TokenPayload;
+        return next();
+    }
+    if (token === undefined) {
+        return res.status(401).json({ message: "not signed in" });
+    }
+    try {
+        const decoded = jwt.verify(token, publicKey) as TokenPayload;
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(400).json({ error });
+    }
+}
+
+async function logRequest(req : Request, res : Response, next : NextFunction) {
+    console.log(`
+		${req.method} ${req.url}
+		Body: ${JSON.stringify(req.body)}
+		Headers: ${JSON.stringify(req.headers, null, 2)}
+	`);
+    next()
+}
+
+export { logRequest, clerkAuth };
--- routes/votes.ts
+++ routes/votes.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import VotesService from '../services/votes';
-import { SchemaValidationError } from 'slonik';
-import { formatQueryErrorResponse, validateRequest } from '../helpers';
+import { validateRequest } from '../helpers';
 import { PendingVote } from '../types/vote';
 import { z } from "zod";
 
@@ -24,15 +23,13 @@ const DeleteRequest = z.object({
   query: RecordRequest
 });
 
-const tempEmail : string = "johndoe@email.com"
-
 router.post("/", validateRequest(PostRequest), async (req, res) => {
 
   const { recordId } = req.validated.query as RecordQuery
   const validationResult = req.validated.body as PendingVote
 
   try{
-    const vote = await VotesService.store(validationResult, recordId, tempEmail);
+    const vote = await VotesService.store(validationResult, recordId, req.user.userEmail);
     return res.status(201).json(vote);
   }catch(e){
     console.log(e)
@@ -43,7 +40,7 @@ router.post("/", validateRequest(PostRequest), async (req, res) => {
 router.delete("/", validateRequest(DeleteRequest), async (req, res) => {
   const { recordId } = req.validated.query as RecordQuery
   try {
-    const rowCount = await VotesService.destroy(recordId, tempEmail);
+    const rowCount = await VotesService.destroy(recordId, req.user.userEmail);
     if (rowCount === 0) {
       return res.status(404).json({message: 'Vote not found'})
     }
