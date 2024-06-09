3e5d5ca66f1b5cbdd6594ab4f843c04dfc317ba9
Nick Guernsey <64866427+nguernse@users.noreply.github.com>
2024-06-09 18:17:29 -0500

    Fix auth bearer (#51)
    * fix: remove colon from auth bearer check
    
    * chore: add a health endpoint

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,29 @@
-import {Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import {TokenPayload} from "./index";
+import { TokenPayload } from "./index";
 
-async function clerkAuth(req : Request, res : Response, next : NextFunction) {
-    const token = req.headers.authorization?.replace("Bearer: ", "");
+const whitelist = [
+    { method: 'GET', route: '/proposals' },
+    { method: 'GET', route: '/health' },
+];
+
+async function clerkAuth(req: Request, res: Response, next: NextFunction) {
+    const token = req.headers.authorization?.replace("Bearer ", "");
     const base64Key = process.env.CLERK_JWT_KEY as string;
     const publicKey = Buffer.from(base64Key, 'base64').toString('ascii');
-    const whitelist = [
-        { method: 'GET', route: '/proposals' }
-    ];
     const isWhitelisted = whitelist.some(
         (item) => item.method === req.method && item.route === req.path
     );
+
     if (isWhitelisted) {
         req.user = {} as TokenPayload;
         return next();
     }
+
     if (token === undefined) {
         return res.status(401).json({ message: "not signed in" });
     }
+
     try {
         const decoded = jwt.verify(token, publicKey) as TokenPayload;
         req.user = decoded;
@@ -28,7 +33,7 @@ async function clerkAuth(req : Request, res : Response, next : NextFunction) {
     }
 }
 
-async function logRequest(req : Request, res : Response, next : NextFunction) {
+async function logRequest(req: Request, res: Response, next: NextFunction) {
     console.log(`
 		${req.method} ${req.url}
 		Body: ${JSON.stringify(req.body)}
