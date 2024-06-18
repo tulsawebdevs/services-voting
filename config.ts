2d37b0d188debd50f1604bc419406734ca098571
rbueno <richardcbueno@gmail.com>
2024-06-18 15:53:23 -0500

    chore(winner): add admin list restriction

--- config.ts
+++ config.ts
@@ -3,6 +3,7 @@ export default {
         'zenlex@zenlex.dev',
         'alec@helmturner.dev',
         'niledixon475@gmail.com',
-        'cryskayecarr@gmail.com'
+        'cryskayecarr@gmail.com',
+        'test_user@test.com'
     ]
 };
