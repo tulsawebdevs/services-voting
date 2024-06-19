c95e368397053950659dba64224f40744a6d1249
rbueno <richardcbueno@gmail.com>
2024-06-18 19:18:32 -0500

    fix: remove test user from default admin list

--- config.ts
+++ config.ts
@@ -1,9 +1,16 @@
-export default {
-    admins: [
-        'zenlex@zenlex.dev',
-        'alec@helmturner.dev',
-        'niledixon475@gmail.com',
-        'cryskayecarr@gmail.com',
-        'test_user@test.com'
-    ]
+import {TEST_USER} from "./helpers";
+
+const admins = [
+    'zenlex@zenlex.dev',
+    'alec@helmturner.dev',
+    'niledixon475@gmail.com',
+    'cryskayecarr@gmail.com'
+]
+
+if (process.env.NODE_ENV === 'test') {
+    admins.push(TEST_USER.userEmail)
+}
+
+module.exports = {
+    admins
 };
