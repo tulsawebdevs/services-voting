d45aee4ac29e1240035da5fb199c2d194ca23d79
ng <n.guerns@gmail.com>
2024-08-01 20:49:18 -0500

    fix: return 404 error for no winning proposals

--- routes/winner.ts
+++ routes/winner.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import WinnerService from '../services/winner'
 import config from '../config'
+import { NotFoundError } from '../helpers';
 
 const router = express.Router();
 
@@ -14,7 +15,9 @@ router.get("/", async (req, res) => {
         const winner = await WinnerService.getWinner();
         return res.status(200).json(winner)
     } catch (e) {
-        console.log(e)
+        if (e instanceof NotFoundError) {
+            return res.status(404).json({ message: e.message });
+        }
         return res.status(500).json({ message: 'Server Error' })
     }
 })
